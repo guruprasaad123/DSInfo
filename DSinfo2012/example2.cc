910af71f8b52b058e8dda53fd23e13ce90b9bacd
guruprasaad123 <jiiva116@live.com>
2019-01-05 10:18:46 +0530

    exercise 1) b)

---
+++ DSinfo2012/example2.cc
@@ -0,0 +1,23 @@
+ #include <iostream> 
+  using namespace std; 
+  int main() 
+    { 
+          int n = 7, k = 8; 
+            cout << ++n << endl; 
+            cout << n << endl; 
+            cout << n++ << endl;
+            cout << n << endl;
+            cout << -n    << endl; 
+            cout << n     << endl; 
+            cout << --n << endl;
+            cout << n << endl; 
+            cout << n-- << endl;
+            cout << n << endl;
+            cout << n + k << endl;
+            cout <<    n    << endl;
+            cout <<    k    << endl; 
+            cout << n << " "<< k << endl; 
+            cout << "n" << endl; 
+            cout << " n * n = "; 
+            cout << n * n << endl; 
+  } 
